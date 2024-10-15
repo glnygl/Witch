f2412002f86588ac57ce30f147e7e7640a7ead82
Glny Gl <glnygl94@gmail.com>
2024-10-15 12:05:41 +0200

    :white_check_mark: Add app icons

---
+++ Witch/Screens/GameDetail/GameDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  GameDetailView.swift
+//  Witch
+//
+//  Created by Glny Gl on 15/10/2024.
+//
+
+import SwiftUI
+
+struct GameDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameDetailView()
+}
---
+++ Witch/Screens/GameList/GameListView.swift
@@ -0,0 +1,18 @@
+//
+//  GameListView.swift
+//  Witch
+//
+//  Created by Glny Gl on 15/10/2024.
+//
+
+import SwiftUI
+
+struct GameListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameListView()
+}
