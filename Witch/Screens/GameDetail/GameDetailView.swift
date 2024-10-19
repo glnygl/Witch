1c6ee914a5053b63fb5e12a536fc565f12fc3d0b
Glny Gl <glnygl94@gmail.com>
2024-10-20 00:12:13 +0200

    :lipstick:  Redesign GameDetailView

--- Witch/Screens/GameDetail/GameDetailView.swift
+++ Witch/Screens/GameDetail/GameDetailView.swift
@@ -11,37 +11,43 @@ import CachedAsyncImage
 struct GameDetailView: View {
     
     @State var viewModel: GameDetailViewModel
-    
+    @State private var showSummary = false
     @State private var showMore = false
     
     var body: some View {
         ScrollView {
-            VStack(alignment: .leading, spacing: 10) {
+            VStack(alignment: .center, spacing: 10) {
                 if let urlString = viewModel.game.cover?.url {
                     CachedAsyncImage(
                         url: GameScreens.detail.url(string: urlString),
                         content: { image in
                             image.resizable()
-                                .frame(height: 400)
+                                .frame(width: 200, height: 200)
                         },
                         placeholder: {
                             Rectangle()
                                 .fill(.gray.opacity(0.4))
-                                .frame(width: 400, height: 400)
+                                .frame(width: 200, height: 200)
                         }
                     )
                     .cornerRadius(40)
                 }
                 Text(viewModel.name)
-                    .font(.title)
+                    .font(.system(size: 24))
                     .fontWeight(.semibold)
                 
                 RatingView(rating: viewModel.convertRating())
                 
-                DisclosureGroup("Summary") {
-                    Text(viewModel.summary)
-                        .font(.subheadline)
-                }.shouldHide(viewModel.summary.isEmpty)
+                DisclosureGroup("Summary", isExpanded: $showSummary) {
+                    VStack {
+                        Text(viewModel.summary)
+                            .font(.subheadline)
+                    }.padding(8)
+                }
+                .padding(EdgeInsets(top: 4, leading: 8, bottom: 4, trailing: 8))
+                
+                .background(showSummary ? .accent.opacity(0.1) : .clear)
+                .shouldHide(viewModel.summary.isEmpty)
                 
                 if let storyline = viewModel.game.storyline {
                     VStack(alignment: .trailing, spacing: 4){
@@ -74,11 +80,12 @@ struct GameDetailView: View {
             }
         }
         .onViewDidLoad(perform: {
+            showSummary = viewModel.summary.count < 400
             Task {
                 guard let ids = viewModel.game.similarGameIds else { return }
                 await viewModel.fetchSimilarGameList(ids: ids)
             }
         })
-        .padding()
+        .padding(EdgeInsets(top: 0, leading: 12, bottom: 6, trailing: 12))
     }
 }
